4f77e2dd410a6d0b3b5d2a26f4eba4a8353d15d7
matheus padilha <mpadilhaac@outlook.com>
2023-03-31 01:05:09 -0300

    atualizado a parte do css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import {BsWind,BsFillDropletFill} from 'react-icons/bs'
+import {HiLocationMarker} from 'react-icons/hi'
+import {FaSearch} from 'react-icons/fa';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className="container">
+
+   <h3>confira o clima de uma cidade:</h3>
+<div className="form">
+
+  <input type="text" placeholder='digite aqui local'/>
+  <button><FaSearch/></button>
+</div>
+<div className="result">
+    <div className="city-status">
+    <span><HiLocationMarker size={30}/></span>
+    <h2>hamburg</h2>
+   <img src="" alt="" />
+    </div>
+
+    <div className="city-results">
+    <span>9%</span>
+    <div className='weather'>
+    <h3>Nublado</h3>
+    <h3>icon</h3>
+    </div>
+   
+    </div>
+<div className="wind-humidity">
+  <div><span><BsFillDropletFill /></span>
+  <p>91%</p></div>
+  
+  <div><span><BsWind/></span>
+  <p>91%</p></div>
+  
+</div>
+</div>
+   </div>
     </div>
   );
 }
