c63ac722b85001b747d747931a8d4122642bb148
matheus padilha <mpadilhaac@outlook.com>
2023-04-03 11:51:06 -0300

    mudanças na logica e no input

--- src/App.js
+++ src/App.js
@@ -1,45 +1,91 @@
+
+import { useState } from 'react';
+
+import axios from 'axios';
+
+
 import {BsWind,BsFillDropletFill} from 'react-icons/bs'
 import {HiLocationMarker} from 'react-icons/hi'
-import {FaSearch} from 'react-icons/fa';
+
 import './App.css';
 
+require('dotenv').config();
+
+const apiKey = process.env.API_KEY;
+
+
 function App() {
-  return (
-    <div className="App">
+
+const [data, setData] = useState('');
+const [location, setLocation] = useState('');
+
+
+
+
+//fetch data
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric&lang=pt_br`;
+
+const searchLocation = (e) => {
+ 
+if(e.key === 'Enter'){
+  axios.get(url).then((res) => {
+    setData(res.data)
+    console.log(data)
+  })
+
+  setLocation("")
+}
+   
+  
+}
+
+
+
+ 
+    return (
+      <div className="App">
    <div className="container">
 
    <h3>confira o clima de uma cidade:</h3>
-<div className="form">
 
-  <input type="text" placeholder='digite aqui local'/>
-  <button><FaSearch/></button>
+    <div className='form' >
+      
+  <input type="text"
+          value={location} 
+          onChange={e => setLocation(e.target.value)}
+          onKeyUp={searchLocation}
+  />
+
 </div>
+
+  
 <div className="result">
+  
     <div className="city-status">
-    <span><HiLocationMarker size={30}/></span>
-    <h2>hamburg</h2>
-   <img src="" alt="" />
+    {data.name ? <h2> <span><HiLocationMarker size={25}/></span> {data.name}</h2> : null }
     </div>
 
     <div className="city-results">
-    <span>9%</span>
+    {data.main ? <p>{data.main.temp}</p> : null}
     <div className='weather'>
-    <h3>Nublado</h3>
-    <h3>icon</h3>
+    {data.weather ? <h3>{data.weather[0].description}</h3> : null}
+ 
     </div>
    
     </div>
-<div className="wind-humidity">
-  <div><span><BsFillDropletFill /></span>
-  <p>91%</p></div>
+    <div className="wind-humidity">
+ 
+  {data.main ? <div><p><BsFillDropletFill /> {data.main.humidity}% </p> </div>: null}
   
-  <div><span><BsWind/></span>
-  <p>91%</p></div>
+  {data.wind ? <div><p><BsWind /> {data.wind.speed} km/h </p> </div>: null}
   
-</div>
-</div>
-   </div>
     </div>
+  </div>
+  </div>
+</div>
+
+   
+    
   );
 }
 
